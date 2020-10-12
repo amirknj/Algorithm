9cf58cfa6d7cf5469ea9fe321fd7aef5f20ace5c
amir konjkav <amirkonjkav@gmail.com>
2020-10-12 17:49:54 +0330

    limit

---
+++ limit.py
@@ -0,0 +1,8 @@
+def limit(arr, min_lim=None, max_lim=None):
+    min_check = lambda val: True if min_lim is None else (min_lim <= val)
+    max_check = lambda val: True if max_lim is None else (val <= max_lim)
+
+    return [val for val in arr if min_check(val) and max_check(val)]
+
+
+print(limit([1, 2, 3, 4, 5], 1, 3))
