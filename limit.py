c8366ea5c66bc345109f159958361f722d25ccb5
amir konjkav <amirkonjkav@gmail.com>
2020-10-17 11:03:01 +0330

    top_one algorithm

--- limit.py
+++ limit.py
@@ -5,4 +5,4 @@ def limit(arr, min_lim=None, max_lim=None):
     return [val for val in arr if min_check(val) and max_check(val)]
 
 
-print(limit([1, 2, 3, 4, 5], 1, 3))
+print(limit([1, 2, 3, 4, 5], None, 3))
---
+++ top_one.py
@@ -0,0 +1,27 @@
+def top(arr):
+    value = {}
+    result = []
+    f_max = 0
+
+    for i in arr:
+        value[i] = value.get(i, 0)+1
+    # for i in arr:
+    #     if i in value:
+    #         value[i] += 1
+    #     else:
+    #         value[i] = 1
+
+    f_max = max(value.values())
+    # f_max = 0
+    # for max_value in value.values():
+    #     if f_max < max_value:
+    #         f_max = max_value
+
+    for i in value.keys():
+        if value[i] == f_max:
+            result.append(i)
+
+    return result
+
+
+print(top([1, 2, 1, 2, 3, 5]))
