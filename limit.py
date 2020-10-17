d73b13b501fb1287bef313e5a54942e9be891060
amirkonjkav <61495812+amirkonjkav@users.noreply.github.com>
2020-10-17 11:11:07 +0330

    Update limit.py

--- limit.py
+++ limit.py
@@ -1,3 +1,4 @@
+#در یک آرایه مقدار مینیمم و ماکزیمم رو دریافت کرده و عناصر بین آن دو رو برمیگرداند  
 def limit(arr, min_lim=None, max_lim=None):
     min_check = lambda val: True if min_lim is None else (min_lim <= val)
     max_check = lambda val: True if max_lim is None else (val <= max_lim)
